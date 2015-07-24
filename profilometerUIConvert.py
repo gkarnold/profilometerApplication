f09894e798b2ec1fc57decb0d40f79578cb572d3
Galen Arnold <galen.arnold@asu.edu>
2015-07-23 17:32:27 -0700

    Just confirmed current version of the UI was correct.

--- profilometerUIConvert.py
+++ profilometerUIConvert.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'profilometer.ui'
 #
-# Created: Tue Jul 21 19:14:02 2015
+# Created: Thu Jul 23 17:28:57 2015
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
